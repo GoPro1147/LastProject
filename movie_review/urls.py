c15ea6889fc732760b8a98a130a2fb005a45b929
고태환 <cooth1147@gmail.com>
2019-11-24 14:15:11 +0900

    createreview, like 기능 구현

--- movie_review/urls.py
+++ movie_review/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('movies/', views.movie_select, name="movie_select"),
     path('movie/<int:id>/', views.movie_detail, name="movie_detail"),
     path('actor/<int:id>/', views.actor_detail, name="actor_detail"),
+    path('<int:id>/create_review/', views.create_review, name="create_review"),
+    path('like/<int:id>/', views.like, name="like"),
     
 ]
--- movie_review/views.py
+++ movie_review/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from .forms import ReviewForm
 from .models import Movie, Actor
 # Create your views here.
 
@@ -16,8 +19,10 @@ def movie_select(request):
 
 def movie_detail(request, id):
     movie = get_object_or_404(Movie, id=id)
+    form = ReviewForm
     context = {
         'movie': movie,
+        'form': form
     }
     return render(request, 'movie_review/detail.html', context)
 
@@ -28,3 +33,29 @@ def actor_detail(request, id):
         'actor': actor
     }
     return render(request, 'movie_review/actor_detail.html', context)
+
+
+@require_POST
+@login_required
+def create_review(request, id):
+    movie = get_object_or_404(Movie, id=id)
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.movie = movie
+        review.user = request.user
+        review.save()
+        return redirect('movie_review:movie_detail', id)
+        
+
+@login_required
+def like(request, id):
+    movie = get_object_or_404(Movie, id=id)
+    user = request.user
+
+    if user in movie.like_users.all():
+        movie.like_users.remove(user)
+    else:
+        movie.like_users.add(user)
+
+    return redirect('movie_review:movie_detail', id)

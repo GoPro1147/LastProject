c527787188680dc676e7e1f1e63902a136ae2764
고태환 <cooth1147@gmail.com>
2019-11-24 10:35:13 +0900

    acounts app setting

--- lastpjt/settings.py
+++ lastpjt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'movie_review',
     'django.contrib.admin',
     'django.contrib.auth',
--- lastpjt/urls.py
+++ lastpjt/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('movie_review.urls'))
+    path('', include('movie_review.urls')),
+    path('accounts', include('accounts.urls'))
 ]

b6558ff692e52e03cd17ea34b17f450a86270452
고태환 <cooth1147@gmail.com>
2019-11-24 11:48:11 +0900

    movie model 수정

--- movie_review/admin.py
+++ movie_review/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie, Actor, Genre, Director
+from .models import Movie, Actor, Genre
 # Register your models here.
 
 admin.site.register(Movie)
--- movie_review/models.py
+++ movie_review/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
-class Director(models.Model):
-    name = models.CharField(max_length=200)
-    img_url = models.CharField(max_length=500)
-
-    def __str__(self):
-        return self.name
 
 class Genre(models.Model):
     genreNm = models.CharField(max_length=100)
@@ -26,10 +20,10 @@ class Movie(models.Model):
     openDt = models.CharField(max_length=100)
     genres = models.ManyToManyField(Genre, related_name='genre_movies')
     actors = models.ManyToManyField(Actor, related_name='filmography')
-    mvdirector = models.ManyToManyField(Director, related_name='filmography')
-    img_url = models.CharField(max_length=500)
-    description = models.CharField(max_length=500)
-    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies', blank=True)
+    mvdirector = models.CharField(max_length=100, blank=True)
+    img_url = models.CharField(max_length=500, blank=True)
+    description = models.CharField(max_length=500, blank=True)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
 
     def __str__(self):
         return self.title
